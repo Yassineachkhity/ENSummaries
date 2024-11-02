c6e16d96c7e02fd3d8c78a6636bc42b339bee1a8
Yassine Achkhity <yassineachkhity56@gmail.com>
2024-11-02 09:20:21 +0100

    Updated styles, HTML, and configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary": "#3b82f6",
+      }
+    },
   },
   plugins: [],
 };
